1117d77e070998a96576934df147b9a0cf2660d9
Vampouille <paul.vampouille@ig2i.centralelille.fr>
2026-02-22 18:22:05 +0100

    //

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ function analyserMETAR($metar){
         }
         //$vent = transformeVent($mots[2]);
     } else {
-        die($mots[2].$mots[3].$mots[4]);
+        die($mots[2]."//".$mots[3]."//".$mots[4]);
         //$vent = transformeVent($mots[3]);
         if(isset($mots[4]) && preg_match("/(\d{3})V(\d{3})/", $mots[4])){
             $vent = transformeVent($mots[3]." ".$mots[4]);
